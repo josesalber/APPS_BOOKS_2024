cb0ff8aaf9d288509359e7e272bc98bb88eb20e5
Jose Salirrosas <vegetamygod@gmail.com>
2024-08-31 19:17:47 -0500

    login y main

---
+++ lib/src/pages/HomePage/Login.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'APPrende+',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('APPrende+'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'INICIA SECION'),
+              Tab(text: 'REGISTRATE'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            LoginTab(),
+            RegisterTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginTab extends StatelessWidget {
+  const LoginTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: 'Usuario',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: 'Contraseña',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          ),
+          const SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: () {
+              // Acción de logeo
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+            child: const Text('ENTRAR'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RegisterTab extends StatefulWidget {
+  const RegisterTab({super.key});
+
+  @override
+  _RegisterTabState createState() => _RegisterTabState();
+}
+
+class _RegisterTabState extends State<RegisterTab> {
+  String selectedInstitution = 'Select';
+  String selectedSchoolYear = '1';
+  String selectedCollegeCycle = '1';
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Nombres',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Apellidos',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            DropdownButtonFormField<String>(
+              value: selectedInstitution,
+              items: ['Select', 'School', 'University']
+                  .map((institution) => DropdownMenuItem(
+                        value: institution,
+                        child: Text(institution),
+                      ))
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  selectedInstitution = value!;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'INSITUCION',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            if (selectedInstitution == 'School')
+              DropdownButtonFormField<String>(
+                value: selectedSchoolYear,
+                items: List.generate(
+                  11,
+                  (index) => DropdownMenuItem(
+                    value: '${index + 1}',
+                    child: Text('Año ${index + 1}'),
+                  ),
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedSchoolYear = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'Año escolar 7 a 11 (1ro a 5to)',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+              ),
+            if (selectedInstitution == 'University')
+              DropdownButtonFormField<String>(
+                value: selectedCollegeCycle,
+                items: List.generate(
+                  10,
+                  (index) => DropdownMenuItem(
+                    value: '${index + 1}',
+                    child: Text('Ciclo ${index + 1}'),
+                  ),
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedCollegeCycle = value!;
+                  });
+                },
+                decoration: InputDecoration(
+                  labelText: 'College Cycle',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+              ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                // Acción de registro
+              },
+              style: ElevatedButton.styleFrom(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+              ),
+              child: const Text('REGISTRAR'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/pages/HomePage/tabs/libros.dart
+++ lib/src/pages/HomePage/tabs/libros.dart
@@ -1,113 +1,244 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class LibrosPage extends StatelessWidget {
+class LibrosPage extends StatefulWidget {
   const LibrosPage({super.key});
 
-  final List<Map<String, String>> books = const [
-    {
-      'title': 'Flutter para Principiantes',
-      'course': 'Desarrollo Móvil',
-      'year': '2024',
-      'author': 'Juan Pérez',
-    },
-    {
-      'title': 'Dart Avanzado',
-      'course': 'Programación',
-      'year': '2023',
-      'author': 'Ana Gómez',
-    },
-    {
-      'title': 'Diseño de Interfaces',
-      'course': 'UI/UX',
-      'year': '2022',
-      'author': 'Carlos López',
-    },
-    // Agrega más libros aquí
-  ];
+  @override
+  _LibrosPageState createState() => _LibrosPageState();
+}
+
+class _LibrosPageState extends State<LibrosPage> {
+  List<dynamic> libros = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchLibros();
+  }
+
+  Future<void> fetchLibros() async {
+    final response = await http.get(
+        Uri.parse('https://www.googleapis.com/books/v1/volumes?q=flutter'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        libros = data['items'];
+      });
+    } else {
+      throw Exception('Error al cargar libros');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: books.map((book) => BookCard(book: book)).toList(),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LIBROS DISPONIBLES!'),
       ),
+      body: libros.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: libros.length,
+              itemBuilder: (context, index) {
+                final libro = libros[index];
+                final volumeInfo = libro['volumeInfo'];
+                final title = volumeInfo['title'];
+                final authors =
+                    volumeInfo['authors']?.join(', ') ?? 'Desconocido';
+                final publishedDate =
+                    volumeInfo['publishedDate'] ?? 'Desconocido';
+                final imageUrl = volumeInfo['imageLinks']?['thumbnail'] ?? '';
+
+                return GestureDetector(
+                  onTap: () {
+                    final previewLink = volumeInfo['previewLink'];
+                    if (previewLink != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetalleLibroPage(
+                            imageUrl: imageUrl,
+                            titulo: title,
+                            autor: authors,
+                            materia: 'Programación',
+                            anio: publishedDate,
+                            previewLink: previewLink,
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[850],
+                      borderRadius: BorderRadius.circular(16.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 10,
+                          offset: const Offset(0, 5),
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        imageUrl.isNotEmpty
+                            ? Image.network(
+                                imageUrl,
+                                height: 150,
+                                fit: BoxFit.cover,
+                              )
+                            : const SizedBox.shrink(),
+                        const SizedBox(width: 16.0),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                title,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                'Autor: $authors',
+                                style: const TextStyle(
+                                  color: Colors.white70,
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                'Publicado: $publishedDate',
+                                style: const TextStyle(
+                                  color: Colors.white70,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
 
-class BookCard extends StatelessWidget {
-  final Map<String, String> book;
+class DetalleLibroPage extends StatelessWidget {
+  final String imageUrl;
+  final String titulo;
+  final String autor;
+  final String materia;
+  final String anio;
+  final String previewLink;
 
-  const BookCard({required this.book, super.key});
+  const DetalleLibroPage({
+    super.key,
+    required this.imageUrl,
+    required this.titulo,
+    required this.autor,
+    required this.materia,
+    required this.anio,
+    required this.previewLink,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: Container(
-        width: 160,
-        padding: const EdgeInsets.all(8.0),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 154, 140, 173),
-          borderRadius: BorderRadius.circular(12.0),
-          boxShadow: [
-            BoxShadow(
-              color: const Color.fromARGB(255, 177, 173, 184).withOpacity(0.2),
-              blurRadius: 6,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titulo),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Imagen del libro
-
-            // Nombre del libro
+            Container(
+              height: 200, // Ajusta el tamaño según lo necesites
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                image: DecorationImage(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit
+                      .contain, // Esto asegura que la imagen se vea completa
+                ),
+              ),
+            ),
+            const SizedBox(height: 16.0),
             Text(
-              book['title']!,
+              titulo,
               style: const TextStyle(
-                fontSize: 16.0,
+                fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
             ),
-            const SizedBox(height: 4.0),
-            // Curso
+            const SizedBox(height: 8.0),
             Text(
-              book['course']!,
+              'Autor: $autor',
               style: const TextStyle(
-                fontSize: 14.0,
-              ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-            ),
-            const SizedBox(height: 4.0),
-            // Año
-            Text(
-              book['year']!,
-              style: const TextStyle(
-                fontSize: 14.0,
+                fontSize: 18.0,
               ),
             ),
-            const SizedBox(height: 4.0),
-            // Autor
+            const SizedBox(height: 8.0),
             Text(
-              book['author']!,
+              'Materia: $materia',
               style: const TextStyle(
-                fontSize: 14.0,
+                fontSize: 18.0,
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              'Año de Publicación: $anio',
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Abrir el enlace de previsualización en el navegador
+                launchURL(previewLink);
+              },
+              child: const Text('Leer en Google Books'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void launchURL(String url) {
+    // Puedes usar el paquete url_launcher para abrir la URL en el navegador
+    // launch(url);
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Libros de Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: const LibrosPage(),
     );
   }
 }
